202f3a46de5dcb27b9348bcebaac32d4892c8eb9
Chien Dat Nguyen Dinh <chiendat@Mac-Dat.local>
2020-10-18 00:58:24 -0300

    Updates

---
+++ precache-manifest.ce7f46c5e1a0960118608dde2f6907ab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43c0685f31d43d658524106a0656537",
+    "url": "/inmyjuniverse/index.html"
+  },
+  {
+    "revision": "ce9b657bf90d20c68d07",
+    "url": "/inmyjuniverse/static/css/main.2797218d.chunk.css"
+  },
+  {
+    "revision": "0534e811bf53e8af1a46",
+    "url": "/inmyjuniverse/static/js/2.fd937406.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inmyjuniverse/static/js/2.fd937406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9b657bf90d20c68d07",
+    "url": "/inmyjuniverse/static/js/main.9e6d51b4.chunk.js"
+  },
+  {
+    "revision": "89c99a8ebaeeb0c825e6",
+    "url": "/inmyjuniverse/static/js/runtime-main.0495f35d.js"
+  },
+  {
+    "revision": "c031d6a71bb95236720417f460acf778",
+    "url": "/inmyjuniverse/static/media/bw-cover.c031d6a7.jpg"
+  },
+  {
+    "revision": "2bc1e57fdce0bbe946963109c91917a6",
+    "url": "/inmyjuniverse/static/media/experimental-cover.2bc1e57f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inmyjuniverse/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4cfec01f5be4c6347db70899f7369e91",
+    "url": "/inmyjuniverse/static/media/people-cover.4cfec01f.jpg"
+  },
+  {
+    "revision": "15f95957e99e2373cff8431aafc6aabf",
+    "url": "/inmyjuniverse/static/media/profilePicture.15f95957.jpg"
+  },
+  {
+    "revision": "0e9b0b63dcd1faa6669fca4edc38c890",
+    "url": "/inmyjuniverse/static/media/travel-cover.0e9b0b63.jpg"
+  },
+  {
+    "revision": "6eb7a60968f02bf8f6d52f0eb1717ad4",
+    "url": "/inmyjuniverse/static/media/urban-cover.6eb7a609.jpg"
+  }
+]);
