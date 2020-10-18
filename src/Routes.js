f7ae545da427f563b8afaa45a5fea8085f2d98c9
Chien Dat Nguyen Dinh <chiendat@Mac-Dat.local>
2020-10-18 01:49:14 -0300

    Changed to hashbrowser

--- src/Routes.js
+++ src/Routes.js
@@ -5,9 +5,7 @@ import Home from "./containers/home/Home";
 export default function Routes() {
     return (
         <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
+            <Route exact path="/" component={Home}/>
         </Switch>
     );
 }
